c86ed427d012e03601599fe2c8e5afd3abbccfdb
Aakash12321 <71495566+Aakash12321@users.noreply.github.com>
2020-11-27 15:40:37 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,28 @@
+class Plinko {
+    constructor(x, y) {
+      var options = {
+          'isStatic':true
+        
+      }
+      this.radius = 10;
+      this.body = Bodies.circle(x, y, 10, options);
+      
+      
+      //this.color=color(random(0, 255), random(0, 255), random(0, 255));
+      World.add(world, this.body);
+    }
+    
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(RADIUS);
+      fill(255);
+      ellipse(0, 0, this.radius, this.radius);
+      pop();
+    }
+  };
+  
